f76cfd8d5571fd8a2ae89c30cf5f204ef0bd9b63
Olaemma-ui <Olaemma4213@gmail.com>
2020-08-18 23:04:08 +0100

    Shoping icon

---
+++ Cart.js
@@ -0,0 +1,10 @@
+var price = document.querySelector('h4').innerHTML;
+var quantityBtn = document.querySelector('#quantity');
+var quantityInput = document.querySelector('#quantity-Input').value;
+
+var goods = [];
+
+function cart () {
+    
+
+}
